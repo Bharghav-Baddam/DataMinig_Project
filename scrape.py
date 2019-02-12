92b8b22c1e6eab7e9ff4338d4f20bd76ecb9f0b8
Zachary Hurst <zhurst31@gmail.com>
2019-02-11 19:34:12 -0500

    Added simple way to collect all the data into a single dictionary

--- scrape.py
+++ scrape.py
@@ -1,6 +1,10 @@
 import csv
+all_data = []
 with open('PlayByPlay.csv', 'rb') as csvfile:
-     spamreader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-     for row in spamreader:
-         print (row['desc'] + "\n")
+     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+     for row in reader:
+         all_data.append(row)
+print("Data Collection Completed")
 
+#  Possible ideas:
+#  Predict for each quarterback, who are they most likely going to pass to when trailing in the 4th quarter
