5fff81a99f50d31c42b35d9b212ecd8f9a50c565
Zachary Hurst <zhurst31@gmail.com>
2019-02-18 17:57:28 -0500

    Fixed wrong informational message

--- group.py
+++ group.py
@@ -3,14 +3,14 @@ import itertools
 
 def group(all_data):
     grouped_data = []
-    print("Data Grouping Completed")
     sorted_data = sorted(all_data, key = takePasser)
     grouped_groupby = groupby(sorted_data, lambda item: item['passer_player_id'])
     for passer_id, grouped_content in grouped_groupby:
         passer_group = []
         for content in grouped_content:
                 passer_group.append(content)
-        grouped_data.append(passer_group)
+        grouped_data.append(passer_group)            
+    print("Data Grouping Completed")
     return grouped_data
 
 def takePasser(elem):
