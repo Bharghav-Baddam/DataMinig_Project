b0fbe7a9bbd24d0a3c7a92f2dd1a6996fd2ee96d
Zachary Hurst <zhurst31@gmail.com>
2019-05-12 14:59:40 -0400

    Association Rules

--- Association_Rule_Direction.py
+++ Association_Rule_Direction.py
@@ -35,69 +35,94 @@ for team in Teams:
       try:
         txtfiles = []
         ls=[]
-        for item in glob.glob(path):
-            txtfiles.append(item)
+        for file in glob.glob(path):
+            txtfiles.append(file)
+        count=-1
 
         for i in txtfiles:
+
             for line in open(i).readlines():
                 data = json.loads(line)
                 for length in range(len(data)):
-                    if int(data[length]['Yards_Away_From_Scoring']) <= 10:
-                        field = "0-10Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 10 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 20:
-                        field = "10-20Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 20 and int(
+                    count += 1
+                    if int(data[length]['Yards_Away_From_Scoring']) > 1 and int(
                             data[length]['Yards_Away_From_Scoring']) <= 30:
-                        field = "20-30Yrds"
+                        field = "0-30Yrds"
                     elif int(data[length]['Yards_Away_From_Scoring']) > 30 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 40:
-                        field = "30-40Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 40 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 50:
-                        field = "40-50Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 50 and int(
                             data[length]['Yards_Away_From_Scoring']) <= 60:
-                        field = "50-60Yrds"
+                        field = "30-60Yrds"
                     elif int(data[length]['Yards_Away_From_Scoring']) > 60 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 70:
-                        field = "60-70Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 70 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 80:
-                        field = "70-80Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 80 and int(
                             data[length]['Yards_Away_From_Scoring']) <= 90:
-                        field = "80-90Yrds"
-                    elif int(data[length]['Yards_Away_From_Scoring']) > 90 and int(
-                            data[length]['Yards_Away_From_Scoring']) <= 100:
+                        field = "60-90Yrds"
+                    else:
                         field = "90-100Yrds"
-                    if data[length]['Passer_Name'] not in 'NA':
-                        ls.append(str(
-                            data[length]['Passer_Name'] +
+                    ls.append(str(
+                            field+','+data[length]['Pass_Direction']+','+ data[length]['Passer_Name']+
                             ',' + data[length]['Receiver_Name']))
+
+
         # write data to the text file: data.basket
-        f = open('data.basket', 'w')
+        f = open('original.basket', 'w')
         for item in ls:
             f.write(item + '\n')
         f.close()
 
         # Load data from the text file: data.basket
-        data = Orange.data.Table("data.basket")
+        data = Orange.data.Table("original.basket")
 
 
         # Identify association rules with supports at least 0.1
         rules = Orange.associate.AssociationRulesSparseInducer(data, support = 0.1)
-        output_file = open("Association_Rules/" + team + "/" + str(quarter) + "/" + str(stat) + "/" + team + "_" + str(quarter) + "_" + str(stat) + "_rules.txt","w+")
-        # write rules to file
+
+
+        # print out rules
+        print "%4s %4s  %s" % ("Supp", "Conf", "Rule")
+        for r in rules[:]:
+            print "%4.1f %4.1f  %s" % (r.support, r.confidence, r)
+
+        ls=[]
+        for line in open('original.basket').readlines():
+            ls.append(line)
+        ls0=[]
+        for i in ls:
+            ls0.append(i.split(','))
+
+        ls1=[]
+        for i in ls0:
+            if(i[2] not in ls1):
+                ls1.append(i[2])
+
+        count=0
+        for i in ls1:
+            count+=1
+            for j in ls0:
+                if(i==j[2]):
+                    f = open('data_'+str(count)+'.basket', 'a')
+                    f.write(j[1]+','+j[2]+','+j[3] + '\n')
+                f.close()
+
+        data = Orange.data.Table("data_1.basket")
+
+
+        # Identify association rules with supports at least 0.2
+        rules = Orange.associate.AssociationRulesSparseInducer(data, support = 0.2)
+
+        output_file = open("Association_Rules/" + team + "/" + str(quarter) + "/" + str(stat) + "/" + team + "_" + str(quarter) + "_" + str(stat) + "_direction.txt","w+")
+        # print out rules
         print "%4s %4s  %s" % ("Supp", "Conf", "Rule")
         output_file.write("%4s %4s  %s" % ("Supp", "Conf", "Rule"))
         output_file.write("\n")
         for r in rules[:]:
-            print "%4.1f %4.1f  %s" % (r.support, r.confidence, r)
             output_file.write("%4.1f %4.1f  %s" % (r.support, r.confidence, r))
             output_file.write("\n")
-        output_file.close()
-        os.remove("data.basket")
+            print "%4.1f %4.1f  %s" % (r.support, r.confidence, r)
+
+        i = 0
+        while i < count:
+            i = i + 1
+            os.remove('data_' + str(i) + '.basket')
+
+        os.remove("original.basket")
       except Exception as ex:
         print ex
         pass
